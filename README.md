2b6cbefd105f9a3316013772130816ca9c1447e2
alligator-io <markus@sonium.org>
2014-12-14 21:52:00 +0100

    initial

---
+++ README.md
@@ -0,0 +1,5 @@
+# icebreaker-peer-net
+
+
+## License
+MIT
---
+++ index.js
@@ -0,0 +1,102 @@
+var net = require('net')
+var fs = require('fs')
+var _ = require('icebreaker')
+require('icebreaker-peer')
+
+var connection = function(original) {
+  original.setKeepAlive(true)
+  original.setNoDelay(true)
+  this.connection(_.net(original))
+}
+
+_.mixin({
+  net : function(connection) {
+    var net = _.pair(connection)
+    if (connection.remoteAddress) net.address = connection.remoteAddress
+    if (connection.remotePort) net.port = connection.remotePort
+    return net
+  }
+})
+
+if (!_.peers) _.mixin({ peers : {} })
+
+_.mixin({
+  net : _.peer({
+    name : 'net',
+    auto : true,
+    start : function() {
+      var server = this.server = net.createServer(connection.bind(this))
+      this.server.on('error', function(err) {
+        if (typeof this.port === 'string' && err.code === 'EADDRINUSE') {
+          var socket = net.Socket()
+
+          socket.on('error', function(err) {
+            if (err.code == 'ECONNREFUSED') {
+              fs.unlink(this.port, function(err) {
+                if (err)
+                  _(
+                    'cannot remove unix socket ' + this.port,
+                    _.log(process.exit.bind(null, 1), 'emerg')
+                  )
+                listen()
+              }.bind(this))
+            }
+          }.bind(this))
+
+          socket.connect(this.port, function() {
+            _(
+              'peer ' + this.name + ' port ' + this.port +
+              ' is already in use by another process.',
+              _.log(process.exit.bind(null, 1), 'emerg')
+            )
+          }.bind(this)
+          )
+
+          return
+        }
+
+        _(
+          [ 'cannot start peer' + this.name + ' on port ' + this.port, err ],
+          _.log(process.exit.bind(null, 1), 'emerg')
+        )
+      }.bind(this))
+
+      var onListening = function() {
+        if (typeof this.port === 'string') fs.chmod(this.port, 0777)
+        this.emit('started')
+      }.bind(this)
+
+      var listen = function(onListening) {
+        this.server.listen(
+          this.port, typeof this.port === 'string' ? null :
+          this.address, onListening
+        )
+      }.bind(this)
+
+      listen(onListening)
+    },
+
+    connect : function(params) {
+      if (!params.address) params.address = this.address
+      process.nextTick(function() {
+        connection.call(this,
+          net.createConnection(typeof params.port === 'string' ? params.port : {
+            port : params.port,
+            host : params.address
+          })
+        )
+      }.bind(this))
+    },
+
+    stop : function() {
+      try {
+        this.server.close(function() {
+          this.emit('stopped')
+        }.bind(this))
+      }
+      catch (e) {
+        _([ e ], _.log(function() { this.emit('stopped') }.bind(this)), 'error')
+      }
+    }
+  })
+}, _.peers)
