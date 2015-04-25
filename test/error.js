1a00daab8690f2a093672d4e0fa33f0c4706d28c
alligator-io <markus@sonium.org>
2015-04-25 13:19:08 +0200

    Update more and better tests

--- test/error.js
+++ test/error.js
@@ -3,26 +3,23 @@ var _ = require('icebreaker')
 require('../index.js')
 
 test('_.peers.net should emit an error',function(t){
-  var p = _.peers.net({port:'./test2.socket'})
-
-  p.on('connection',function(connection){
+  _.peers.net({port:'./test2.socket'})
+  .on('connection',function(connection){
+    var self = this
     console.log('connection ',connection.address,':',connection.port)
-    _(_.empty(),connection,_.onEnd(function(err){
+    _(['test1','test1','test1','test1'],connection,_.onEnd(function(err){
       t.equal(err.code,'ECONNREFUSED')
-      p.stop()
+      self.stop()
     }))
   })
-
-  p.on('started',function(){
-    p.connect({address:'localhost',port:'9384'})
+  .on('started',function(){
+    this.connect({address:'localhost',port:'9384'})
   })
-
-  p.on('stopped',function(){
+  .on('stopped',function(){
     t.equal(this.port,'./test2.socket')
     t.equal(this.name,'net')
-    t.equal(Object.keys(p.connections).length,0)
+    t.equal(Object.keys(this.connections).length,0)
     t.end()
   })
-
-  p.start()
+  .start()
 })
